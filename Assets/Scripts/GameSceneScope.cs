4e2dcd0ede6d41d1e136777495ede8233553bc45
kroonhorstdino <coder.kroonhorstdino@outlook.de>
2023-02-03 22:51:44 +0100

    Add struct for player references

---
+++ Assets/Scripts/GameSceneScope.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class GameSceneScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        var player = GameObject.FindWithTag("Player");
+        
+        builder.RegisterInstance<PlayerReferences>(new()
+        {
+            Hitpoints = player.GetComponent<Hitpoints>()
+        });
+    }
+}
+
+/// <summary>
+/// Stores references to player components
+/// </summary>
+public struct PlayerReferences
+{
+    public Hitpoints Hitpoints;
+}
